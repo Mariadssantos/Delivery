5e89778b26bef9191ce66fa7c157d4c1f10b244c
Maria Eduarda Santos <mariaeduardasanntoss@gmail.com>
2023-01-06 20:56:56 -0300

    config route authenticate

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
 
 const routes = Router();
 
+const authenticateClientController = new AuthenticateClientController();
 const createClientController = new CreateClientController();
+
+routes.post("/authenticate", authenticateClientController.handle)
 routes.post("/client/", createClientController.handle)
 
 export {routes};
